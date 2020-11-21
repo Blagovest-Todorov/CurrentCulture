f6aa1c79fdfa643ee5dd7ea4699da14bceef6ad7
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-21 07:36:09 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.FormattingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(System.Threading.Thread.CurrentThread.CurrentCulture);
+            //Showing the current culture
+            Console.WriteLine(DateTime.Now);
+
+            //
+            //now changing the current culture//setting a new culture settings
+            System.Threading.Thread.CurrentThread.CurrentCulture =
+                new System.Globalization.CultureInfo("bg-BG"); //as we create new object of a class
+            Console.WriteLine(DateTime.Now);
+
+
+            
+        }
+    }
+}
